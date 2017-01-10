8c7d98610c76485d3edc010f82486039a480a8b1
Hexatorn <pawel.niemczak@gmail.com>
2017-01-10 20:15:23 +0100

    v. 0.0.1
    Pliki Projektu Cube - p[ierwszy comit

---
+++ src/hexatorn/Point.java
@@ -0,0 +1,26 @@
+/**
+ * Created by hexatorn on 2016-10-16.
+ */
+package hexatorn;
+
+public class Point {
+
+    int x;
+    int y;
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
